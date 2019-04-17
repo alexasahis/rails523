befcc39d10ff604cdc6cdee16330dbeaa15e7184
alex <alex@paradise>
2019-04-17 02:55:38 +0000

    setup puma & nginx

--- config/puma.rb
+++ config/puma.rb
@@ -4,16 +4,17 @@
 # the maximum value specified for Puma. Default is set to 5 threads for minimum
 # and maximum; this matches the default thread size of Active Record.
 #
-threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
-threads threads_count, threads_count
+threads_count = ENV.fetch("RAILS_MAX_THREADS") { 16 }
+threads 1, threads_count
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
 #
-port        ENV.fetch("PORT") { 3000 }
+#port        ENV.fetch("PORT") { 3000 }
 
 # Specifies the `environment` that Puma will run in.
 #
-environment ENV.fetch("RAILS_ENV") { "development" }
+#environment ENV.fetch("RAILS_ENV") { "development" }
+environment ENV.fetch("RAILS_ENV") { "production" }
 
 # Specifies the number of `workers` to boot in clustered mode.
 # Workers are forked webserver processes. If using threads and workers together
@@ -21,7 +22,22 @@ environment ENV.fetch("RAILS_ENV") { "development" }
 # Workers do not work on JRuby or Windows (both of which do not support
 # processes).
 #
-# workers ENV.fetch("WEB_CONCURRENCY") { 2 }
+# Use cluster mode and set the number of workers to 1.5x the number of cpu cores in the machine, minimum 2.
+# grep -c processor /proc/cpuinfo --> 2
+workers ENV.fetch("WEB_CONCURRENCY") { 3 }
+
+app_dir = File.expand_path("../..", __FILE__)
+shared_dir = "#{app_dir}/tmp"
+
+# Set up socket location
+bind "unix://#{shared_dir}/puma.sock"
+
+# Logging
+stdout_redirect "#{app_dir}/log/puma.stdout.log", "#{app_dir}/log/puma.stderr.log", true
+
+# Set master PID and state locations
+pidfile "#{shared_dir}/puma.pid"
+state_path "#{shared_dir}/puma.state"
 
 # Use the `preload_app!` method when specifying a `workers` number.
 # This directive tells Puma to first boot the application and load code
@@ -32,3 +48,16 @@ environment ENV.fetch("RAILS_ENV") { "development" }
 
 # Allow puma to be restarted by `rails restart` command.
 plugin :tmp_restart
+
+#on_worker_boot do
+#	ActiveSupport.on_load(:active_record) do
+#		ActiveRecord::Base.establish_connection
+#	end
+	#require "active_record"
+  #ActiveRecord::Base.connection.disconnect! rescue ActiveRecord::ConnectionNotEstablished
+  #ActiveRecord::Base.establish_connection(YAML.load_file("#{app_dir}/config/database.yml")[rails_env])
+#end
+
+before_fork do
+	# configuration here
+end
